4517b62aa7ac5072763e81e6fbf609437021a6b3
Katarzyna Malek <kmal@softserveinc.com>
2017-10-03 13:13:46 +0200

    add time format using moments.js

--- src/jobs/jobs.js
+++ src/jobs/jobs.js
@@ -1,9 +1,12 @@
+var moment = require('moment');
+
 export default function createJobs(jobs){
     const jobsList = document.querySelector('ul.jobs');
+    const jobsTime = moment(jobs.time).toString();
     
     const jobsHTML =
         `<h4>${jobs.title}</h4><p><a href="${jobs.url}">(${jobs.url})</a></p>
-        <div class="jobs__post-info"><p class="time">${jobs.time}hours ago</p></div>`;
+        <div class="jobs__post-info"><p class="time">${jobsTime}</p></div>`;
 
     const jobsElement = document.createElement('li');
     jobsElement.innerHTML = jobsHTML;
