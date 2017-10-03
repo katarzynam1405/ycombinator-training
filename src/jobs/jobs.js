98e60447d25cab9d4b1c5e763dc92dadd42990e7
Katarzyna Malek <kmal@softserveinc.com>
2017-10-03 12:57:56 +0200

    add job offert template

--- src/jobs/jobs.js
+++ src/jobs/jobs.js
@@ -2,8 +2,8 @@ export default function createJobs(jobs){
     const jobsList = document.querySelector('ul.jobs');
     
     const jobsHTML =
-        `<h4>Irure aliquip culpa velit labore quis id amet laboris qui qui adipisicing et dolore quis.</h4><p><a href="#">(smithsonianmag.com)</a></p>
-        <div class="jobs__post-info"><p class="time">x hours ago</p></div>`;
+        `<h4>${jobs.title}</h4><p><a href="${jobs.url}">(${jobs.url})</a></p>
+        <div class="jobs__post-info"><p class="time">${jobs.time}hours ago</p></div>`;
 
     const jobsElement = document.createElement('li');
     jobsElement.innerHTML = jobsHTML;
