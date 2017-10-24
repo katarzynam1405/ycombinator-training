951d6b5098052bae8a7950fbe0a733ffab17c7d2
Katarzyna Malek <kmal@softserveinc.com>
2017-10-24 12:20:35 +0200

    add pagination to search jobs

--- src/searchEngine/searchJobs.js
+++ src/searchEngine/searchJobs.js
@@ -4,8 +4,12 @@ export default function searchJobs() {
     const input = document.querySelector('#search-input.jobs');
     const jobs = Array.from(jobsList.querySelectorAll('li'));
 
+    const paginationList = document.querySelector('.pagination');
+    const serchedPrePage = 5;
+
     input.addEventListener('input', function (event) {
         let inputValue = event.target.value.toLowerCase();
+        paginationList.innerHTML = "";
 
         if (inputValue === null) {
             return;
@@ -19,8 +23,27 @@ export default function searchJobs() {
             return job.innerHTML.toLowerCase().indexOf(inputValue) !== -1
         });
         
-        jobsSerachResults.forEach((jobsSerachResult)=> jobsSerachResult.classList.remove('hidden'));
-
         document.querySelector('button.button.more-jobs').style.display = "none";
-    });
+
+        paginationList.classList.remove('hidden');
+
+        for(let i = 0; i< (jobsSerachResults.length/serchedPrePage); i++) {
+            paginationList.innerHTML += `<span data-value="${i}"> [${i + 1}] </span>`;
+        }
+
+        let page = 0;
+        const spans = paginationList.querySelectorAll('span');
+        for(let i = 0; i <spans.length; i++) {
+        spans[i].addEventListener('click', function(event){
+            page = parseInt(event.target.dataset.value);
+            pagination(page, jobsSerachResults);
+        });
+    }
+});
+
+    function pagination(page, jobsSerachResults) {
+        const results = jobsSerachResults.slice((page * serchedPrePage), ((page + 1)*serchedPrePage));
+        console.log(results);
+        results.forEach((result) => result.classList.remove('hidden'));
+    }
 };
