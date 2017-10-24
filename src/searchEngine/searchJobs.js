2b7e9d019b7be59c5c50d1437823ed0ee7ab777d
Katarzyna Malek <kmal@softserveinc.com>
2017-10-24 10:53:08 +0200

    complete pagination

--- src/searchEngine/searchJobs.js
+++ src/searchEngine/searchJobs.js
@@ -3,23 +3,24 @@ export default function searchJobs() {
     const jobsList = document.querySelector('ul.jobs');
     const input = document.querySelector('#search-input.jobs');
     const jobs = Array.from(jobsList.querySelectorAll('li'));
-    console.log('jobs');
+
     input.addEventListener('input', function (event) {
         let inputValue = event.target.value.toLowerCase();
 
         if (inputValue === null) {
             return;
         } else {
-            for (var i = 0; i < jobs.length; i++) {
-                jobs[i].classList.add('hidden');
-            }
+            jobs.forEach((job)=>{
+                 job.classList.add('hidden');
+            })
         }
 
-        for (var i = 0; i < jobs.length; i++) {
-            if (jobs[i].innerHTML.toLowerCase().indexOf(inputValue) !== -1) {
-                jobs[i].classList.remove('hidden');
-            }
-        }
+        const jobsSerachResults = jobs.filter((job)=>{
+            return job.innerHTML.toLowerCase().indexOf(inputValue) !== -1
+        });
+        
+        jobsSerachResults.forEach((jobsSerachResult)=> jobsSerachResult.classList.remove('hidden'));
+
         document.querySelector('button.button.more-jobs').style.display = "none";
     });
 };
--- src/searchEngine/searchNews.js
+++ src/searchEngine/searchNews.js
@@ -1,32 +1,46 @@
 export default function searchNews(newsList) {
     const input = document.querySelector('#search-input.news');
 
-    const news = Array.from(newsList.querySelectorAll('li'));
-    const pagination = document.querySelector('.pagination');
+    const news = Array.from(newsList.querySelectorAll('li[data-id]'));
+    const paginationList = document.querySelector('.pagination');
+    const serchedPrePage = 5;
+    
 
     input.addEventListener('input', function (event) {
+        document.querySelector('button.button.more-news').style.display = 'none';
+        paginationList.innerHTML = "";
         let inputValue = event.target.value.toLowerCase();
 
         if (inputValue === null) {
             return;
         } else {
-            for (var i = 0; i < news.length; i++) {
-                news[i].classList.add('hidden');
-            }
+            news.forEach((oneNews) => oneNews.classList.add('hidden'));
         }
 
         const searchResults = news.filter((oneNews)=>{
             return oneNews.innerHTML.toLowerCase().indexOf(inputValue) !== -1
-        })
+        });
+        //show pagination list on start searching
+        paginationList.classList.remove('hidden');
 
-        searchResults.forEach((searchResult) => searchResult.classList.remove('hidden'));
-
-        console.log(searchResults);
-
-        if(searchResults.length >= 20){
-            pagination.classList.remove('hidden');
+        //create dynamic pagination newsList
+        for(let i = 0; i< (searchResults.length/serchedPrePage); i++) {
+            paginationList.innerHTML += `<span data-value="${i}"> [${i + 1}] </span>`;
         }
-     
-        document.querySelector('button.button.more-news').style.display = 'none';
+
+        //run pagination after click on paginationList
+        let page = 0;
+        const spans = paginationList.querySelectorAll('span');
+        for(let i = 0; i <spans.length; i++) {
+        spans[i].addEventListener('click', function(event){
+            page = parseInt(event.target.dataset.value); //parseInt to be sure that taken value is a integer number
+            pagination(page, searchResults);
+        });
+    }
     });
+
+    function pagination(page, searchResults) {
+        const results = searchResults.slice((page * serchedPrePage), ((page + 1)*serchedPrePage));
+        results.forEach((searchResult) => searchResult.classList.remove('hidden'));
+    }
 };
