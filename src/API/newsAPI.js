1c316899a9728676d700f49e9baff3a097684dc4
Katarzyna Malek <kmal@softserveinc.com>
2017-10-26 13:31:05 +0200

    disable more button if there is no more news

--- src/API/newsAPI.js
+++ src/API/newsAPI.js
@@ -2,12 +2,14 @@ export default function getNewsApi(page){
     return new Promise((resolve, reject)=>{
         document.querySelector('button.button.more-news').setAttribute('disabled', 'disabled');
         resolve(
-            fetch(`http://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=20&page=${page}&format=json`)
+            fetch(`https://hn.algolia.com/api/v1/search?query=tags=story&hitsPerPage=20&page=${page}&format=json`)
             .then((response)=>{
              return response.text()
             })
             .then(function(stories){
-                document.querySelector('button.button.more-news').removeAttribute('disabled');
+                if(JSON.parse(stories).hits.length != 0) {
+                    document.querySelector('button.button.more-news').removeAttribute('disabled');
+               }
                 return stories ? JSON.parse(stories):{}; 
             }))
     })}
